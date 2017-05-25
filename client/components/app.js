f67f76b01ca82f60777ac421b39ab0814c4393d5
jordanpina <jordan.t.pina@gmail.com>
2017-05-24 18:25:52 -0700

    merge to master2

--- client/components/app.js
+++ client/components/app.js
@@ -147,32 +147,3 @@ class App extends Component {
 
 
 module.exports = App;
-
-
-
-// import React, { Component } from 'react';
-// import axios from 'axios';
-// import Recipe from './recipe.js';
-// import { Link } from 'react-router-dom';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
-//
-//
-
-// class Nav extends Component {
-//   constructor(props) {
-//     super(props)
-//   }
-//
-//   render() {
-//     return (
-//       <div className="navBarDiv">
-//         <Link className="navBarLinks" to="/">Login</Link>
-//         <Link className="navBarLinks" to="/signup">Register</Link>
-//         <Link className="navBarLinks" to="/profile">Profile</Link>
-//         <Link className="navBarLinks" to="/search">Search Recipes</Link>
-//       </div>
-//     )
-//   }
-// }
-//
-// module.exports = Nav;
